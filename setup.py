df7addfc34e5528375e9319d9556d2dd6999c14f
Mathias Bynens <mathias@qiwi.be>
2016-10-13 02:35:40 +0200

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
 	name = 'pogocpm2level',
 	packages = ['pogocpm2level'],
-	version = '0.0.0',
+	version = '1.0.0',
 	download_url = 'https://github.com/mathiasbynens/pogocpm2level/tarball/v1.0.1',
 	description = 'Easily calculate the level of a given Pokémon in Pokémon GO based on its total CP multiplier value.',
 	author = 'Mathias Bynens',
